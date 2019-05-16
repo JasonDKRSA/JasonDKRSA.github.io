dbafdcb4119e07203bc08a3a3cd8a2c3cd907a07
JasonDKRSA <jklaaste@gmail.com>
2019-05-17 01:11:06 +0200

    Added missing files

---
+++ precache-manifest.64463c7c49436fdf9c37ad59dbb65f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17282ff02135ede362302518700b4177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e59fc493876e09d8f218",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f8348ac27a7d6ee001b3",
+    "url": "/static/js/2.f1c924c2.chunk.js"
+  },
+  {
+    "revision": "e59fc493876e09d8f218",
+    "url": "/static/js/main.c46b0f33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
