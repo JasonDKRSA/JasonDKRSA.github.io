e38b03fc4805916d7c5c39f412c3f74f2c87db2c
JasonDKRSA <jklaaste@gmail.com>
2019-05-20 01:29:10 +0200

    Added missing stuff

---
+++ precache-manifest.39cc33795a207b82173db3207923f119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6172ccc1866f5baa9c01f2387242171a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933fff69a2a4c11c0bb4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4ad0ec9e4703d6d55fc2",
+    "url": "/static/js/2.9d0ffda1.chunk.js"
+  },
+  {
+    "revision": "933fff69a2a4c11c0bb4",
+    "url": "/static/js/main.8ed841b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
